d75e29eeb5d38f29580af717eb12ebce5301e35a
XLLU <lucasluu@yahoo.com>
2020-11-08 18:06:00 +0800

    Clean as structure

--- src/app/post/post.router.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import * as postController from './post.controller';
-import { printRequestUrl } from '../app.middleware';
-
-const router = express.Router();
-
-router.get('/posts', printRequestUrl, postController.index);
-
-export default router;
