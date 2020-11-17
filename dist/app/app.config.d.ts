5e4ab8480a0c46bfeb36e990e1bfec3150e4ff35
XLLU <lucasluu@yahoo.com>
2020-11-17 10:40:09 +0800

    Update .gitignore, Clear Cache

--- dist/app/app.config.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const APP_PORT: string;
-export declare const MYSQL_HOST: string, MYSQL_PORT: string, MYSQL_USER: string, MYSQL_PASSWORD: string, MYSQL_DATABASE: string;
-export declare let PRIVATE_KEY: string, PUBLIC_KEY: string;
